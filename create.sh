60470339214c1176b1d50ac4d73db0739d30a201
Gamerx20003 <jeganramamurthyv@gmail.com>
2021-08-26 00:20:40 +0530

    4

--- create.sh
+++ create.sh
@@ -4,6 +4,7 @@ cp mobile.loadbalance.local.conf /etc/httpd/conf/extra
 cp desktop.loadbalance.local.conf /etc/httpd/conf/extra
 
 systemctl restart httpd
+
 echo "127.0.0.1       mobile.loadbalance.local 
 127.0.0.1       desktop.loadbalance.local" >> /etc/hosts
 
